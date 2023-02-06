7ab45f624df937d4db4ac84670de5ba869e790b0
FerniGG <FerniGG@github.com>
2023-02-06 23:57:17 +0100

    comienzo del poligono piramide ¿¿camara z??

--- Browser/browser_gobj.cc
+++ Browser/browser_gobj.cc
@@ -197,31 +197,46 @@ void create_scene_tmesh() {
 	mesh->addPoint(Vector3(0.25+offset, -0.25+offset, -0.5)); // P5
 	mesh->addPoint(Vector3(0.25+offset, 0.25+offset, -0.5 )); // P6
 	mesh->addPoint(Vector3(-0.25+offset, 0.25+offset, -0.5 )); // P7
-
+	mesh->addPoint(Vector3(0,0,offset));//Piramid center
 	// front
-	mesh->addTriangle(0, 1, 2); // P0 - P1 - P2
-	mesh->addTriangle(2, 3, 0); // P2 - P3 - P0
+	//mesh->addTriangle(0, 1, 2); // P0 - P1 - P2
+	//mesh->addTriangle(2, 3, 0); // P2 - P3 - P0
 
 	/* =================== PUT YOUR CODE HERE ====================== */
 	// Complete the triangle mesh with the vertices and triangles that
 	// form a 3D cube.
-	mesh->addTriangle(0, 1, 2);
-	mesh->addTriangle(0, 1, 2);
+	
+	//base
+	mesh->addTriangle(0, 1, 2); //se añade el triangulo creado por 
+	//el punto 0,1,2 que son los 
+	//que ocupan las posiciones 0, 1 y 2 
+	//respectivamente en la red de triangulos
+	mesh->addTriangle(0, 3, 2);mesh->addTriangle(0, 3, 2);
 
+	//piramid sizes
+	mesh->addTriangle(0, 1, 8);
+	mesh->addTriangle(1, 2, 8);
+	mesh->addTriangle(2, 3, 8);
+	mesh->addTriangle(0, 3, 8);
+/*
 	mesh->addTriangle(5, 4, 7);
 	mesh->addTriangle(7, 6, 5);
 
+	//lado izq
 	mesh->addTriangle(1, 5, 6);
 	mesh->addTriangle(6, 2,1);
 
+	//lado drch no sale porque se "tapa"
 	mesh->addTriangle(4,0,3);
 	mesh->addTriangle(3,7,4);
-
+	
+	//top
 	mesh->addTriangle(3,2,6);
 	mesh->addTriangle(6,7,3);
 
+
 	mesh->addTriangle(4,5,1);
-	mesh->addTriangle(1,0,4);
+	mesh->addTriangle(1,0,4);*/
 	/* =================== END YOUR CODE HERE ====================== */
 
 	GObject *gObj = GObjectManager::instance()->create("MG_CUBE");
@@ -240,7 +255,28 @@ void create_regular_polygon(int N) {
 	/* =================== PUT YOUR CODE HERE ====================== */
 	// Create a regular polygon of N sides.
 
+	float offset = 0.25;
+	mesh->addPoint(Vector3(-0.25, -0.25, -0.25 )); // P0
+	mesh->addPoint(Vector3(0.25, -0.25, -0.25 )); // P1
+	mesh->addPoint(Vector3(0.25, 0.25, -0.25 )); // P2
+	mesh->addPoint(Vector3(-0.25, 0.25, -0.25 )); // P3
 
+	mesh->addPoint(Vector3(-0.25+offset, -0.25+offset, -0.5 )); // P4
+	mesh->addPoint(Vector3(0.25+offset, -0.25+offset, -0.5)); // P5
+	mesh->addPoint(Vector3(0.25+offset, 0.25+offset, -0.5 )); // P6
+	mesh->addPoint(Vector3(-0.25+offset, 0.25+offset, -0.5 )); // P7
+	mesh->addPoint(Vector3(0,0,offset));
+	
+	mesh->addTriangle(0, 1, 2); //se añade el triangulo creado por 
+	//el punto 0,1,2 que son los 
+	//que ocupan las posiciones 0, 1 y 2 
+	//respectivamente en la red de triangulos
+	mesh->addTriangle(0, 3, 2);
+
+	mesh->addTriangle(0, 1, 8);
+	mesh->addTriangle(1, 2, 8);
+	mesh->addTriangle(2, 3, 8);
+	mesh->addTriangle(0, 3, 8);
 	/* =================== END YOUR CODE HERE ====================== */
 
 	GObject *gObj = GObjectManager::instance()->create("MG_POLYGON");
